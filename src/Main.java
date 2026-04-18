74076655f002da23773ce57a61f6e20b3f6ed658
IbrahimCSKhader <ik2907951@gmail.com>
2026-04-18 23:43:37 +0300

    push first task training

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Student s1 = new Student("karem", 25);
+        s1.addGrade(90);
+        s1.addGrade(85);
+        s1.addGrade(70, 5);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Student s2 = new Student("ibrahim", 21);
+        s2.addGrade(89);
+        s2.addGrade(84);
+
+        Student s3 = new Student("massara", 22);
+
+        List<Student> students = Arrays.asList(s1, s2, s3);
+
+        System.out.println("Students information \n \n \n");
+        students.forEach(student -> student.print());
+
+        System.out.println("Print all student names using forEach");
+        students.forEach(student -> System.out.println(student.getName()));
+
+        System.out.println("1 Students with average > 80 ");
+        students.stream()
+                .filter(student -> student.calculateAverage() > 80)
+                .forEach(student ->
+                        System.out.println(student.getName() + " -> " + student.calculateAverage()));
+
+        System.out.println("average of all students ");
+        double allAverage = students.stream()
+                .mapToDouble(Student::calculateAverage)
+                .average()
+                .orElse(0.0);
+
+        System.out.println("all Average: " + allAverage);
+
+        System.out.println("Filter grades > 80 for s1 ");
+        List<Integer> filteredGrades = s1.filterGrades(g -> g > 80);
+        System.out.println(filteredGrades);
+
+        System.out.println(" Student Report");
+        Student.Report report = s1.new Report();
+        report.printReport();
+
+        System.out.println("the getClass");
+        Class<?> c = s1.getClass();
+
+        System.out.println("Class Name: " + c.getName());
+
+        Method[] methods = c.getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println(method.getName());
         }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,61 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class Student {
+public class Student extends Person implements Printable {
+    private List<Integer> grades;
+
+    public Student() {
+        this.grades = new ArrayList<>();
+    }
+
+    public Student(String n, int a) {
+        super(n, a);
+        this.grades = new ArrayList<>();
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    public void addGrade(int grade, int bonus) {
+        grades.add(grade + bonus);
+    }
+
+    public double calculateAverage() {
+        double avg = 0;
+       avg= grades.isEmpty()? 0 :grades.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+
+        return avg;
+    }
+
+    public List<Integer> getGrades() {
+        return new ArrayList<>(grades);
+    }
+
+    public List<Integer> filterGrades(GradeFilter filter) {
+        return grades.stream()
+                .filter(g -> filter.test(g))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + "\nStudent  Grades: " + grades  + "\n Student Average: " + calculateAverage();
+    }
+
+    @Override
+    public void print() {
+        System.out.println(getDescription());
+    }
+
+    public class Report {
+        public void printReport() {
+            System.out.println("Student Name: " + getName() + "\nAverage Grade: " + calculateAverage());
+        }
+    }
 }
